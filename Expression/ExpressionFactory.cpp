d9787ef049c58950b57c57dae1a17f3bb670b9d2
yuliaZas <yulia.az.24@gmail.com>
2018-12-25 23:47:12 +0200

    Expression Factory

--- Expression/ExpressionFactory.cpp
+++ Expression/ExpressionFactory.cpp
@@ -7,56 +7,19 @@
 ExpressionFactory::ExpressionFactory() {}
 
 Expression* ExpressionFactory::expressionEvaluate(string exp) {
-    char prevVal;
-    vector<string> *strVec  = stringExpToVec(exp);
 
-    for (string token : *strVec) {
-        cout << token << endl;
-    }
-    /*
-    for (int i = 0; i < exp.length() ; i++) {
-        if (exp[i] == ' ') {
-            continue;
-        }
-        else if (exp[i] >= '0' || exp[i] <= '9') {
-            this->queue1.push(exp[i]);
-        }
-        else if (exp[i] == '.') {
-//            TODO this caes
-            if (prevVal >= '0' || prevVal <= '9' ) {}
-        }
-        else if (isOparator(exp[i])) {
-            priorOp(exp[i]);
-        }
-        else if (exp[i] == '(') {
-            this->stack1.push(exp[i]);
-        }
-        else if (exp[i] == ')') {
-            if (this->stack1.top() == '(') {
-                this->stack1.pop();
-            }
-            else {
-                this->queue1.push(this->stack1.top());
-                this->stack1.pop();
-            }
-        }
-        prevVal = exp[i];
-    }
-    for (int j = 0; j < this->stack1.size() ; ++j) {
-        if (isOparator(this->stack1.top())) {
-            this->queue1.push(this->stack1.top());
-            this->stack1.pop();
-        }
-    }
-    this->expression = queueToExp();
-     */
-    this->expression;
+    vector<string> strVec  = stringExpToVec(exp);
+
+    vector<string> strPostfix = stringToPostfix(strVec);
+
+    this->expression = getExp(strPostfix);
+
+    return this->expression;
 }
 
-vector<string>* ExpressionFactory::stringExpToVec(string str) {
+vector<string> ExpressionFactory::stringExpToVec(string str) {
     string withoutSpaces;
     vector<string> tokenVec;
-    vector<string>* retVec;
 
     //making a string with out spaces
     for (int i = 0; i < str.length() ; ++i) {
@@ -93,7 +56,6 @@ vector<string>* ExpressionFactory::stringExpToVec(string str) {
             if (withoutSpaces[j] == '-') {
                 if (isOparator(withoutSpaces[j-1])) {
                     temp += withoutSpaces[0];
-                    j = 1;
                     isNeg = true;
                 }
             }
@@ -102,6 +64,11 @@ vector<string>* ExpressionFactory::stringExpToVec(string str) {
                 tokenVec.push_back(temp);
                 temp = "";
             }
+        }
+        else if (withoutSpaces[j] == '(' || withoutSpaces[j] == ')') {
+            temp += withoutSpaces[j];
+            tokenVec.push_back(temp);
+            temp = "";
         }
         else {
             while (isDig(withoutSpaces[j]) || withoutSpaces[j] == '.') {
@@ -113,8 +80,124 @@ vector<string>* ExpressionFactory::stringExpToVec(string str) {
             temp = "";
         }
     }
-    *retVec = tokenVec;
-    return retVec;
+    return tokenVec;
+}
+
+vector<string> ExpressionFactory::stringToPostfix(vector<string> strVec) {
+    vector<string> resultArray;
+    for (string token : strVec) {
+        if (isNum(token)) {
+            this->queue1.push(token);
+        }
+        else if (isOparatorStr(token)) {
+            string curOperator = token;
+
+            /* While there is an operator with greater precedence in the stack, pop it to the queue */
+            if (!this->stackOp.empty()) {
+                string top = this->stackOp.top();
+                while (!this->stackOp.empty() && priorOp(top, curOperator)) {
+                    this->stackOp.pop(); //pop the operator from the stack
+                    this->queue1.push(top); //add the operator to the queue
+                    //top = this->stackOp.top();
+                }
+            }
+            //push the current operator onto the stack
+            this->stackOp.push(curOperator);
+        }
+        else if (token == "(") {
+            this->stackOp.push(token);
+        }
+        else if (token == ")") {
+            /* While there's not a left bracket at the top of the stack
+              Pop operators from the stack onto the output queue.*/
+            while (!this->stackOp.empty() && (this->stackOp.top() != "(")) {
+                string top = this->stackOp.top();
+                this->queue1.push(top);
+                this->stackOp.pop();
+
+            }
+            //pop the left bracket from the stack and discard it
+            this->stackOp.pop();
+        }
+    }
+    //while there are operators on the stack, pop them to the queue
+    while (!this->stackOp.empty()) {
+        string top = this->stackOp.top();
+        if (top != "(" && top != ")") {
+            this->queue1.push(top);
+            this->stackOp.pop();
+        }
+    }
+
+    //move the queue to the array
+    while (!this->queue1.empty()) {
+        resultArray.push_back(this->queue1.front());
+        this->queue1.pop();
+    }
+    return resultArray;
+}
+
+Expression* ExpressionFactory::getExp(vector<string> str) {
+    stack<Expression*> expressionStack;
+    for (string token : str) {
+        //If the current string is an operator
+        if (isOparatorStr(token)) {
+            char oper = token[0];
+            //take two numbers out of the stack
+            Expression* num2 = expressionStack.top();
+            expressionStack.pop();
+            Expression* num1 = expressionStack.top();
+            expressionStack.pop();
+
+            //create the expression with the two numbers
+            Expression* exp;
+            switch (oper) {
+                case '+':
+                    exp = new Plus(num1, num2);
+                    break;
+                case '-':
+                    exp = new Minus(num1, num2);
+                    break;
+                case '*':
+                    exp = new Mul(num1, num2);
+                    break;
+                case '/':
+                    exp = new Div(num1, num2);
+                    break;
+                case '>':
+                    if (token.length() > LEN_OP) { // ">="
+                        exp = new BiggerEqual(num1, num2);
+                    }
+                    else {
+                        exp = new Bigger(num1, num2);
+                    }
+                    break;
+                case '<':
+                    if (token.length() > LEN_OP) { // "<="
+                        exp = new SmallerEqual(num1, num2);
+                    }
+                    else {
+                        exp = new Smaller(num1, num2);
+                    }
+                    break;
+                case '=': //"=="
+                    exp = new Equal(num1, num2);
+                    break;
+                default:
+                    throw "Operator is unknown";
+            }
+            //Push the created expression to the stack;
+            expressionStack.push(exp);
+        } else { //The current string is a number
+            //convert the string to double and push it the stack
+            expressionStack.push(new Number(stod(token)));
+        }
+    }
+    //There should be one item in the stack which is the return value.
+    if (expressionStack.size() != 1) {
+        throw "Something went wrong with the algorithm.";
+    }
+    return expressionStack.top();
 }
 
 bool ExpressionFactory::isDig(char ch) {
@@ -125,58 +208,45 @@ bool ExpressionFactory::isDig(char ch) {
 }
 
 bool ExpressionFactory::isOparator(char op) {
-    if (op == '+') { return true; }
-    else if (op == '-') { return true; }
-    else if (op == '*') { return true; }
-    else if (op == '/') { return true; }
-    else { return false; }
-}
-
-void ExpressionFactory::priorOp(char op) {
-    if (op == '+' || op == '-'){
-        if (this->stack1.top() == '*') {
-            //this->queue1.push(this->stack1.top());
-            this->stack1.pop();
-            this->stack1.push(op);
-        }
-        else if (this->stack1.top() == '/') {
-            //this->queue1.push(this->stack1.top());
-            this->stack1.pop();
-            this->stack1.push(op);
-        }
-        else {
-            this->stack1.push(op);
-        }
-    }
-    else if (op == '*' || op == '/') {
-        this->stack1.push(op);
-    }
-    return;
-}
-/*
-Expression* ExpressionFactory::queueToExp() {
-    stack<Node> stackNodes;
-    if (this->queue1.front()>= '0' || this->queue1.front() <= '9') {
-        Expression* number = new Number(this->queue1.front() - '0');
-        this->queue1.pop();
-        return number;
-    }
-    switch (this->queue1.back()) {
-        case '+':
-            this->queue1.pop();
-
-            break;
-        case '-':
-
-            break;
-        case '*':
-
-            break;
-        case '/':
-
-            break;
-        default:
-            return nullptr;
+    if (op == '+' || op == '-' || op == '*' || op == '/' || op == '=' || op == '==' ||
+            op == '<' || op == '<=' || op == '>' || op == '>=') {
+        return true;
+    } else {
+        return false;
     }
 }
- */
+
+bool ExpressionFactory::isOparatorStr(string str) {
+    if (str == "+" || str == "-" || str == "*" || str == "/" || str == "=" ||
+            str == "==" || str == "<" || str == "<=" || str == ">" || str == ">=") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool ExpressionFactory::priorOp(string curr, string top) {
+    if (curr == "*" || curr == "/") {
+        if (top == "+" || top == "-") {
+            return true;
+        }
+    }
+    if ((curr == ">" || curr == "<" || curr == "==" || curr == "<=" || curr == ">=")) {
+        if (top == "*" || top == "/" || top == "+" || top == "-") {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+bool ExpressionFactory::isNum(string str) {
+    bool ret = true;
+    for (int i = 0; i < str.length(); ++i) {
+        if (!isDig(str[i])) {
+            ret = false;
+        }
+    }
+    return ret;
+}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,79 @@
 #include "Parser.h"
 #include "Lexer.h"
 #include "Expression/ExpressionFactory.h"
+#include "Expression/Expression.h"
+#include "list"
 
 int main() {
 	//check factory
 	ExpressionFactory x = ExpressionFactory();
+	list < Expression * > list1;
+	Expression* c = x.expressionEvaluate("3 * - 10");
+	cout<< c->calculate()<<endl;
+	list1.push_back(c);
+	/*
+Expression* c = x.expressionEvaluate(" + -  40");
+Expression* c = x.expressionEvaluate("5 - + - 3");
+Expression* c = x.expressionEvaluate("5 + - + 3");
 
-	Expression* a = x.expressionEvaluate("10+2");
-	Expression* c = x.expressionEvaluate("-10+-8*3");
-	Expression* b = x.expressionEvaluate("100 * 2 + 12");
+
+	list1.push_back(e0);
+	Expression* e1 = x.expressionEvaluate("5 - - 3");
+	list1.push_back(e1);
+	Expression* e2 = x.expressionEvaluate("3 - + 10");
+	list1.push_back(e2);
+	Expression* e22 = x.expressionEvaluate("3 + - 10");
+	list1.push_back(e22);
+	Expression* e3 =
+	list1.push_back(e3);
+	Expression* e31 = x.expressionEvaluate("3 * + 10");
+	list1.push_back(e31);
+	Expression* e32 = x.expressionEvaluate("81 / - 9");
+	list1.push_back(e32);
+	Expression* e33 = x.expressionEvaluate("81 / + 9");
+	list1.push_back(e33);
+	Expression* f = x.expressionEvaluate(" - ( - 2 + 4)");
+	list1.push_back(f);
+	Expression* g = x.expressionEvaluate(" - ( - 6 / - 2)");
+	list1.push_back(g);
+	Expression* h = x.expressionEvaluate(" - ( - 6 * - 2)");
+	list1.push_back(h);
+	Expression* i = x.expressionEvaluate(" 0.2 * 0.5 ");
+	list1.push_back(i);
+	Expression* i1 = x.expressionEvaluate(" (0.8 -(0.2 * 0.5) + 0.8 ) / 0.5 ");
+	list1.push_back(i1);
+	Expression* i2 = x.expressionEvaluate(" 1/3 ");
+	list1.push_back(i2);
+	Expression* i3 = x.expressionEvaluate(" 1 / 3 / 1 / 4 / 1 / 5");
+	list1.push_back(i3);
+	Expression* i4 = x.expressionEvaluate(" (1 / 3)* (1 / 4) * (1 / 5) ");
+	list1.push_back(i4);
+	*/
+
+/*output:
+ * 100
+ * -40
+ * 8
+ * 2
+ * 8
+ * -7
+ * -7
+ * -30
+ * 30
+ * -9
+ * 9
+ * -2
+ * -3
+ * -12
+ * 0.1
+ * 3
+ * 0.333333
+ * 0.166667
+ * 0.166667
+ * */
+
+
+
     // check git
 	vector<string> args;
 	//args.insert(args.end(), "var");
